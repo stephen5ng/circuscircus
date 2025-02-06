396343f349a9a2fc486e6c19d858205483fb7d18
Stephen Ng <stephen5.ng@gmail.com>
2025-02-06 15:00:28 -0800

    wrote it

--- circus.py
+++ circus.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python
 
 import pygame
+from pygame import Color
 import pygame.freetype
-
+import hub75
 
 SCALING_FACTOR = 6
 SCREEN_WIDTH = 128
 SCREEN_HEIGHT = 32
 
 pygame.init()
-pygame.freetype.init()
+hub75.init()
 
-pygame.font.get_init()
+pygame.freetype.init()
 
 display_surface = pygame.display.set_mode(
    (SCREEN_WIDTH*SCALING_FACTOR, SCREEN_HEIGHT*SCALING_FACTOR))
 
 pygame.display.set_caption('Circus Circus')
 
-font2 = pygame.font.SysFont('COURIER', 15)
+font_guess = pygame.freetype.Font("bitmap-fonts/bitmap/raize/raize-13.pcf", 13)
+font_small = pygame.freetype.Font("bitmap-fonts/bitmap/scientifica/scientifica-11.bdf", 11)
 guess = ""
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-text2 = font2.render(alphabet, True, (0, 255, 0))
-textRect2 = text2.get_rect()
 
 screen = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+shifted = {
+   "1": "!",
+   "/": "?",
+   "-": "_",
+   "=": "+"
+}
 while True:
    screen.fill((0, 0, 0))
 
-   screen.blit(text2, textRect2)
+   font_guess.render_to(screen, (0, 0), guess[:16], Color("green"), Color("black"))
+   font_guess.render_to(screen, (0, 11), guess[16:], Color("green"), Color("black"))
+   font_small.render_to(screen, (0, 23), alphabet[:8], Color("red"), Color("black"))
+   font_small.render_to(screen, (40, 23), alphabet[8:], Color("red"), Color("black"))
 
    for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
          key = pygame.key.name(event.key).upper()
-         print(f"{event.key} {pygame.key.name(event.key)} {pygame.K_ESCAPE}")
+         print(f"{key} {event.key} {pygame.key.name(event.key)} {pygame.K_ESCAPE}")
          if event.key == pygame.K_ESCAPE:
             guess = ""
             alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -45,7 +54,12 @@ while True:
             if key in alphabet:
                guess += key
                alphabet = alphabet.replace(key, ' ')
-               text2 = font2.render(alphabet, True, (255, 255, 255))
+            else:
+               shift_bits = pygame.KMOD_LSHIFT | pygame.KMOD_RSHIFT
+               if pygame.key.get_mods() & shift_bits and key in shifted.keys():
+                  guess += shifted[key]
+               else:
+                  guess += key
 
       if event.type == pygame.QUIT:
          pygame.quit()
@@ -54,3 +68,4 @@ while True:
       pygame.transform.scale(screen,
           display_surface.get_rect().size, dest_surface=display_surface)
       pygame.display.update()
+      hub75.update(screen)
